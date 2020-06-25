4dac236d494dff04dbe82104c568db62f4089d6c
HyungMoYang <42955111+HyungMoYang@users.noreply.github.com>
2020-06-25 18:20:50 +0900

    render html page

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("index")
+    return render(request, 'shareRes/index.html') # request를 받아서 'templates/shareRes/index.html' render
+
+def restaurantDetail(request):
+    # return HttpResponse("restaurantDetail")
+    return render(request, 'shareRes/restaurant_detail.html')
+
+def restaurantCreate(request):
+    # return HttpResponse("restaurantCreate")
+    return render(request, 'shareRes/restaurantCreate.html')
+    
+
+def categoryCreate(request):
+    # return HttpResponse("categoryCreate")
+    return render(request, 'shareRes/categoryCreate.html')

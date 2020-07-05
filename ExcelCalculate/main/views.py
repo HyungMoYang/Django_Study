9ced532228f4f8900bb1ca8a3406e95f9f4ef3cd
HyungMoYang <42955111+HyungMoYang@users.noreply.github.com>
2020-07-05 21:26:07 +0900

    1

--- ExcelCalculate/main/views.py
+++ ExcelCalculate/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import * # DB model 추가
+from random import * # for 난수 처리
+from sendEmail.views import * # sendEmail app의 views.py의 모든것 import
 
 # Create your views here.
 # 메인 화면 - index
@@ -11,13 +13,25 @@ def signup(request):
     return render(request, 'main/signup.html')
 
 def join(request): # 인증코드 입력 화면
-    print(request)
+    # print(request)
     name = request.POST['signupName']
     email = request.POST['signupEmail']
     pw = request.POST['signupPW']
     user = User(user_name = name, user_email = email, user_password = pw)
-    user.save()
-    return redirect('main_verifyCode')
+    user.save() # html file에서 위의 정보를 받아와서 DB에 저장
+
+    # 4자리 인증 코드를 위한 난수 처리
+    code = randint(1000, 9999)
+    response = redirect('main_verifyCode') # redirect를 위한 페이지를 리턴하지 않고 저장, 쿠기 저장을 위함. 
+    response.set_cookie('code', code) # 해당 페이지 쿠키를 세팅한다. 
+    response.set_cookie('user_id', user.id)
+    
+    # 이메일 발송 함수 호출
+    send_result = send(email, code) # sendEmail의 함수 호출
+    if send_result: 
+        return response
+    else:
+        return HttpResponse("이메일 발송에 실패했습니다.")
 
 # 로그인 처리
 def signin(request):
@@ -28,7 +42,22 @@ def verifyCode(request):
     return render(request, 'main/verifyCode.html')
 
 def verify(request):
-    return redirect('main_index')
+    user_code = request.POST['verifyCode'] # 해당 url의 request에서 user가 입력한 코드 값을 받아옴. 
+    cookie_code = request.COOKIES.get('code') # 저장된 쿠기에서 코드 값을 가져옴 -> 현재 26줄에 있는 코드 
+
+    # 코드 유효 검사 
+    if user_code == cookie_code:
+        user = User.objects.get(id = request.COOKIES.get('user_id')) # DB에서 쿠키에 저장된 user_id 값과 같은 값을 불러온다. 
+        user.user_validate = 1 # 아이디 유효하게 변경
+        user.save() # DB에 새로 저장
+        response = redirect('main_index') # 페이지 저장 
+        response.delete_cookie('code') # 기존 쿠키 삭제
+        response.delete_cookie('user_id')
+        response.set_cookie('user', user) # 새 쿠키 저장
+        return response
+    else:
+        redirect('main_verifyCode')
+
 
 def result(request):
     return render(request, 'main/result.html')
--- ExcelCalculate/sendEmail/views.py
+++ ExcelCalculate/sendEmail/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
 
+# 여기가 이해 잘 안됨..
 # Create your views here.
-# def send(receiverEmail, verifyCode):
-#     return HttpResponse("sendEmail, send function!")
-
-def send(request):
-    return HttpResponse("sendEmail, send function!")
+def send(receiverEmail, verifyCode):
+    try:
+        content = {'verifyCode': verifyCode}
+        msg_html = render_to_string('sendEmail/email_format.html', content)
+        msg = EmailMessage(subject="인증 코드 발송 메일", body = msg_html, from_email="tester001@gmail.com", bcc=[receiverEmail])
+        msg.content_subtype = 'html'
+        msg.send()
+        return True
+    except:
+        return False
